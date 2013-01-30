8f4106bdd5155f453633e57ba8c8ceb451e8a860
Josh Reisner <josh@joshreisner.com>
2013-01-30 15:47:03 -0800

    some work today on header

--- header.php
+++ header.php
@@ -11,3 +11,23 @@
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_directory');?>/css/global.css">
 	</head>
 	<body <?php body_class();?>>
+		<div class="header_wrapper">
+			<div class="wrapper2">
+				<header>
+					<a class="logo" href="/">Poverty & Homelessness <span>NYC</span></a>
+					
+					<nav class="nav">
+						<a href="/">Timeline</a>
+						<a href="/eras/" class="eras">Eras</a>
+						<a href="/maps/" class="maps">Maps</a>
+						<a href="/browse/" class="browse">Browse</a>
+						<a href="/about/" class="about">About</a>
+					</nav>
+					
+					<nav class="tools">
+						<a href="#">Share</a>
+						<a href="#">Search</a>
+					</nav>
+				</header>
+			</div>
+		</div>
