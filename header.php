ce22e0cdd65614db45e99d994166f864d1c16c81
Josh Reisner <josh@joshreisner.com>
2013-02-07 12:53:09 -0800

    adding header stuff from before

--- header.php
+++ header.php
@@ -22,51 +22,51 @@
 					<div id="nav">
 						<div class="menu">
 							<ul>
-								<li class="current_page_item">
+								<li<?php if ($_SERVER['REQUEST_URI'] == '/') echo ' class="current_page_item"'?>>
 									<a href="/">Timeline</a>
 								</li>
 								<li>
 									<a href="/eras/">Eras</a>
-										<ul class="children">
-											<li>
-												<a href="/eras/early-new-york-city/">
-													1650s to 1830s: Family Poverty in Early New York City
-												</a>
-											</li>
-											<li>
-												<a href="/eras/19th-century/">
-													1830s to 1890s: Poverty &#038; Homelessness in the 19th Century
-												</a>
-											</li>
-											<li>
-												<a href="/eras/the-progressive-era/">
-													1890s to 1920s: Poverty &#038; Homelessness in the Progressive Era
-												</a>
-											</li>
-											<li>
-												<a href="/eras/the-great-depression/">
-													1929 to 1945: Family Crises in the Great Depression
-												</a>
-											</li>
-											<li>
-												<a href="/eras/new-urban-poverty/">
-													1945 to Today: The Origin of New Urban Poverty
-												</a>
-											</li>
-										</ul>
-									</li>
-									<li>
-										<a href="/maps/">Maps</a>
-									</li>
-									<li>
-										<a href="/topics/">Topics</a>
-									</li>
-									<li>
-										<a href="/about/">About</a>
-									</li>
-								</ul>
-							</div>
+									<ul class="children">
+										<li>
+											<a href="/eras/early-new-york-city/">
+												1650s to 1830s: Family Poverty in Early New York City
+											</a>
+										</li>
+										<li>
+											<a href="/eras/19th-century/">
+												1830s to 1890s: Poverty &#038; Homelessness in the 19th Century
+											</a>
+										</li>
+										<li>
+											<a href="/eras/the-progressive-era/">
+												1890s to 1920s: Poverty &#038; Homelessness in the Progressive Era
+											</a>
+										</li>
+										<li>
+											<a href="/eras/the-great-depression/">
+												1929 to 1945: Family Crises in the Great Depression
+											</a>
+										</li>
+										<li>
+											<a href="/eras/new-urban-poverty/">
+												1945 to Today: The Origin of New Urban Poverty
+											</a>
+										</li>
+									</ul>
+								</li>
+								<li<?php if ($_SERVER['REQUEST_URI'] == '/maps/') echo ' class="current_page_item"'?>>
+									<a href="/maps/">Maps</a>
+								</li>
+								<li<?php if ($_SERVER['REQUEST_URI'] == '/topics/') echo ' class="current_page_item"'?>>
+									<a href="/topics/">Topics</a>
+								</li>
+								<li<?php if ($_SERVER['REQUEST_URI'] == '/about/') echo ' class="current_page_item"'?>>
+									<a href="/about/">About</a>
+								</li>
+							</ul>
 						</div>
+					</div>
 				</header>
 			</div>
 		</div>
