04a57d6bca1eb577e7ac6fe0af837367d375a7b0
Josh Reisner <josh@joshreisner.com>
2013-02-09 15:15:11 -0800

    round of additions to timeline, adjustments to footer just to get all the elements more or less on the page.

--- footer.php
+++ footer.php
@@ -1,3 +1,12 @@
+		<footer>
+			<div class="copyright">
+				<img src="<?php bloginfo('template_directory');?>/img/footer/logo.png" alt="logo" width="229" height="27" />
+			</div>
+			<div class="social">
+				<img src="<?php bloginfo('template_directory');?>/img/footer/social.png" alt="social" width="103" height="21" />
+			</div>
+		</footer>
+
 		<?php wp_footer(); ?>
 		<script src="<?php bloginfo('template_directory');?>/js/javascript.js"></script>
 	</body>
