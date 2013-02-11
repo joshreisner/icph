9868203789fc460c85f2ae3cb475535ab9b268a8
Josh Reisner <josh@joshreisner.com>
2013-02-11 13:02:35 -0800

    morning work on era page, header

---
+++ page-timeline.php
@@ -0,0 +1,80 @@
+<?php
+//blog page
+get_header();
+?>
+<div class="timeline_mask">
+	<ul id="timeline">
+		<li class="progressive overview">
+			<div class="upper">
+				<h1>1890&dash;1928</h1>
+				<h2>The Progressive Era</h2>
+			</div>
+			<div class="lower">
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+			</div>
+		</li>
+		<li class="progressive featured">
+			<div class="upper"></div>
+			<div class="lower">
+				<a href="#gordon-family"><img src="<?php bloginfo('template_directory');?>/img/placeholder/gordon-family-s.jpg" alt="gordon-family-s" width="283" height="169" /></a>
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic. Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+			</div>
+		</li>
+		<li class="progressive">
+			<div class="upper">
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="125" height="125" />
+				<h3>1898</h3>
+			</div>
+			<div class="lower">
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+			</div>
+		</li>
+		<li class="progressive">
+			<div class="upper">
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="125" height="125" />
+				<h3>1898</h3>
+			</div>
+			<div class="lower">
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+			</div>
+		</li>
+		<li class="progressive">
+			<div class="upper">
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="125" height="125" />
+				<h3>1898</h3>
+			</div>
+			<div class="lower">
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+			</div>
+		</li>
+	</ul>
+	
+	<ul id="slider">
+		<li class="early_ny">
+			1650 <span>Early New York</span>
+		</li>
+		<li class="nineteenth">
+			1830 <span>19th Century</span>
+		</li>
+		<li class="progressive active">
+			1890 <span>The Progressive Era</span>
+		</li>
+		<li class="great_depression">
+			1929 <span>Great Depression</span>
+		</li>
+		<li class="today">
+			1945 <span>Today</span>
+		</li>
+	</ul>
+</div>
+
+<?php
+get_footer();
---
+++ page.php
@@ -0,0 +1,125 @@
+<?php
+//era landing pages
+get_header();
+?>
+
+<div id="era" class="progressive">
+
+	<div class="row header">
+		<div class="inner">
+			<h1>1890&ndash;1928</h1>
+			<h2>The Progressive Era</h2>
+			<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+			<a class="left" href="#">Continue Era Introduction</a>
+			<a class="right" href="#">Browse the Timeline</a>
+		</div>
+	</div>
+	
+	<div class="row feature_policies">
+		<div class="inner">
+			<div class="column left feature">
+				<h3>The Gordon Family</h3>
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/gordon-era.jpg" alt="gordon-era" width="160" height="229" />
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<a class="more">The Gordon Family</a>
+			</div>
+			<div class="column right policies">
+				<h3>Policy in this Time Period</h3>
+				<ul>
+					<li class="heading first">Low Income Housing</li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+
+					<li class="heading">Low Income Housing</li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+
+					<li class="heading">Low Income Housing</li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+
+					<li class="heading">Low Income Housing</li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+					<li><a href="#">Tenement House Act of 1901</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row map">
+		<div class="inner">
+			<h3>Where were the Settlement Houses?</h3>
+			<a href="#" class="more">Explore the 1900s Map</a>
+		</div>
+	</div>
+	
+	<div class="row topics">
+		<div class="inner">
+			<div class="column left">
+				<h3>The Growth of NYC</h3>
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<ul class="related">
+					<li>
+						<a href="#" class="img"><img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="150" height="150" /></a>
+						<a href="#" class="text">New Amsterdam</a>
+					</li>
+					<li class="center">
+						<a href="#" class="img"><img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="150" height="150" /></a>
+						<a href="#" class="text">New Amsterdam</a>
+					</li>
+					<li>
+						<a href="#" class="img"><img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="150" height="150" /></a>
+						<a href="#" class="text">New Amsterdam</a>
+					</li>
+				</ul>
+			</div>
+			<div class="column right">
+				<h3>Children Welfare</h3>
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<ul class="related">
+					<li>
+						<a href="#" class="img"><img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="150" height="150" /></a>
+						<a href="#" class="text">New Amsterdam</a>
+					</li>
+					<li class="center">
+						<a href="#" class="img"><img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="150" height="150" /></a>
+						<a href="#" class="text">New Amsterdam</a>
+					</li>
+					<li>
+						<a href="#" class="img"><img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="150" height="150" /></a>
+						<a href="#" class="text">New Amsterdam</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row stats">
+		<div class="inner">
+			<h3>By the Numbers</h3>
+			<ul>
+				<li>
+					<div class="stat">2/3 of New York City's population live in tenements</div>
+					<div class="img"><img src="<?php bloginfo('template_directory');?>/img/placeholder/stat1.png" alt="stat1" width="227" height="170" /></div>
+				</li>
+				<li>
+					<div class="stat">Total number of child workers in the United States in 1900: 1.7 million</div>
+					<div class="img"><img src="<?php bloginfo('template_directory');?>/img/placeholder/stat2.png" alt="stat1" width="267" height="170" /></div>
+				</li>
+				<li>
+					<div class="stat">National economic output increased by 85%</div>
+					<div class="img"><img src="<?php bloginfo('template_directory');?>/img/placeholder/stat3.png" alt="stat1" width="85" height="170" /></div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	
+</div>
+
+<?php
+get_footer();
+?>
