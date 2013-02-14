8a1eaa60261df73d64e75cf70f6594e6787ecc60
Josh Reisner <josh@joshreisner.com>
2013-02-13 16:23:52 -0800

    resizing timeline

--- page-timeline.php
+++ page-timeline.php
@@ -10,16 +10,93 @@ get_header();
 				<h2>The Progressive Era</h2>
 			</div>
 			<div class="lower">
-				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
-				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
-				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+				<p>Urbanization, immigration, and industrialization transformed New York City's economy between 1890 and 1920, making poverty more prevalent among the working class while at the same time creating enormous wealth for some.</p>
+				<p>Efforts to alleviate the effects of poverty among working-class and poor families through direct action and government reform become known as "progressivism."</p>
+				<p>The sights, sounds, and smells of the poorer districts in New York City in the 1890s made evident the effects of mass urbanization, immigration, and industrialization.</p>
 			</div>
 		</li>
 		<li class="progressive featured">
 			<div class="upper"></div>
 			<div class="lower">
 				<a href="#gordon-family"><img src="<?php bloginfo('template_directory');?>/img/placeholder/gordon-family-s.jpg" alt="gordon-family-s" width="283" height="169" /></a>
-				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic. Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p><a href="">March 12, 1907</a> — West 28th St.<br>Storm water poured from the ceiling of the basement apartment and down its plaster walls, soaking the family’s meager bed, dresser, and table before coming to rest in deep, dirty puddles on the floor.  Maria Gordon’s family—her nine-year-old niece, Edith, and six month-old foster child, Perry—had nowhere to sleep, and the workspace where Maria laundered clothes for her clients was unusable.</p>
+			</div>
+		</li>
+		<li class="progressive">
+			<div class="upper">
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="125" height="125" />
+				<h3>1898</h3>
+			</div>
+			<div class="lower">
+				<p><a href="#gordon-family">Lillian Wald</a> and Mary Brewster open the Henry Street Settlement on the Lower East Side.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+			</div>
+		</li>
+		<li class="progressive">
+			<div class="upper">
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="125" height="125" />
+				<h3>1898</h3>
+			</div>
+			<div class="lower">
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+			</div>
+		</li>
+		<li class="progressive">
+			<div class="upper">
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="125" height="125" />
+				<h3>1898</h3>
+			</div>
+			<div class="lower">
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+			</div>
+		</li>
+		<li class="progressive">
+			<div class="upper">
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="125" height="125" />
+				<h3>1898</h3>
+			</div>
+			<div class="lower">
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+			</div>
+		</li>
+		<li class="progressive">
+			<div class="upper">
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="125" height="125" />
+				<h3>1898</h3>
+			</div>
+			<div class="lower">
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+			</div>
+		</li>
+		<li class="progressive">
+			<div class="upper">
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="125" height="125" />
+				<h3>1898</h3>
+			</div>
+			<div class="lower">
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
+			</div>
+		</li>
+		<li class="progressive">
+			<div class="upper">
+				<img src="<?php bloginfo('template_directory');?>/img/placeholder/great-migration-circle.png" alt="great-migration-circle" width="125" height="125" />
+				<h3>1898</h3>
+			</div>
+			<div class="lower">
+				<p>Neutra PBR tousled before they sold out, 90's aesthetic readymade quinoa helvetica aliqua veniam authentic. Anim vegan nostrud vero. Flexitarian beard plaid irure four loko, banh mi pitchfork 3 wolf moon quis before they sold out. Freegan fanny pack vero ut, skateboard terry richardson assumenda sunt irure farm-to-table organic.</p>
+				<p>Vegan readymade fashion axe polaroid sapiente. Fugiat squid scenester, etsy before they sold out synth williamsburg qui keytar voluptate hella neutra. Iphone next level banh mi bespoke authentic, disrupt laborum skateboard gentrify single-origin coffee american apparel incididunt keffiyeh.</p>
+				<p>Quis vero fanny pack helvetica portland fashion axe, officia art party echo park. Beard flannel aesthetic ad. Gluten-free readymade thundercats, eu disrupt tonx narwhal brunch wayfarers cardigan.</p>
 			</div>
 		</li>
 		<li class="progressive">
