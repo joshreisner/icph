98691c561c4f1a4d2fefbf18cbe0205012417437
Josh Reisner <josh@joshreisner.com>
2013-02-16 16:43:43 -0800

    tightening up css with variables for dimensions

--- page-maps.php
+++ page-maps.php
@@ -2,7 +2,7 @@
 //maps page
 get_header();
 ?>
-<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/?ie=UTF8&amp;ll=40.733565,-73.983608&amp;spn=0.091314,0.181789&amp;t=t&amp;z=13&amp;output=embed"></iframe>
+<iframe src="https://maps.google.com/?ie=UTF8&amp;ll=40.733565,-73.983608&amp;spn=0.091314,0.181789&amp;t=t&amp;z=14&amp;output=embed"></iframe>
 <?php
 get_footer();
 ?>
