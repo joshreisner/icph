f65124ee09a69ba64e6b7a655583227c0eabc350
Josh Reisner <josh@joshreisner.com>
2013-03-14 21:40:40 -0700

    customizing google maps with lots of javascript

--- page-maps.php
+++ page-maps.php
@@ -3,37 +3,102 @@
 $body_class = 'maps';
 get_header();
 ?>
-    <script src="//maps.google.com/maps/api/js?sensor=false"></script>
+    <script src="//maps.google.com/maps/api/js?key=AIzaSyADB0wyHm58AKLfOefVvZ13G-oZq3UftWY&sensor=false"></script>
     <script src="//maptilercdn.s3.amazonaws.com/klokantech.js"></script>
 
     <style>
-      html, body, #map { width:100%; height:100%; margin:0; padding:0; }
+	    html, body, #map { width:100%; height:100%; margin:0; padding:0; }
     </style>
 
     <div id="map"></div>
 
 	<script>
-		var map;
-		var mapMinZoom = 10;
-		var mapMaxZoom = 15;
+		var map = new google.maps.Map(document.getElementById('map'), {
+			streetViewControl: false,
+			panControl: false,
+			zoomControl: false
+		});
 		var mapBounds = new google.maps.LatLngBounds(
 			new google.maps.LatLng(40.682437, -74.027215),
 			new google.maps.LatLng(40.880905, -73.901394)
 		);
+		map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
+		map.fitBounds(mapBounds);
+
+		//workaround to fix zoom
+		zoomChangeBoundsListener = google.maps.event.addListenerOnce(map, 'bounds_changed', function() {
+        	this.setZoom(14);
+        	this.setCenter(new google.maps.LatLng(40.725, -73.965));
+		});
+		setTimeout(function(){google.maps.event.removeListener(zoomChangeBoundsListener)}, 2000);
+
+		var controlDiv = document.createElement('div');
+		controlDiv.style.padding = '70px 20px';
+	
+		var zoomIn = document.createElement('div');
+		zoomIn.innerHTML = '+';
+		zoomIn.style.color = 'white';
+		zoomIn.style.textAlign = 'center';
+		zoomIn.style.lineHeight = '50px';
+		zoomIn.style.fontSize = '20px';
+		zoomIn.style.backgroundColor = 'black';
+		zoomIn.style.width = '50px';
+		zoomIn.style.height = '50px';
+		zoomIn.style.cursor = 'pointer';
+		controlDiv.appendChild(zoomIn);
+
+		var zoomOut = document.createElement('div');
+		zoomOut.innerHTML = '-';
+		zoomOut.style.color = 'white';
+		zoomOut.style.textAlign = 'center';
+		zoomOut.style.lineHeight = '50px';
+		zoomOut.style.fontSize = '20px';
+		zoomOut.style.backgroundColor = 'black';
+		zoomOut.style.width = '50px';
+		zoomOut.style.height = '50px';
+		zoomOut.style.marginTop = '10px';
+		zoomOut.style.cursor = 'pointer';
+		controlDiv.appendChild(zoomOut);
+	
+		map.controls[google.maps.ControlPosition.TOP_LEFT].push(controlDiv);
+
+		//map.setZoom(13);
+		google.maps.event.addDomListener(zoomIn, 'click', function() {
+			var zoom = map.getZoom();
+			map.setZoom(zoom + 1);
+		});
+		
+		google.maps.event.addDomListener(zoomOut, 'click', function() {
+			var zoom = map.getZoom();
+			map.setZoom(zoom - 1);
+		});
+		
+		var mapMinZoom = 10;
+		var mapMaxZoom = 15;
 		var mapGetTile = function(x,y,z) { 
 			return "/wp-content/themes/icph/img/eras/progressive/tiles/" + z + "/" + x + "/" + y + ".png";
 		}
 
-		var opts = {
-			streetViewControl: false,
-			center: new google.maps.LatLng(0,0),
-			zoom: 14 //10
-		};
-		map = new google.maps.Map(document.getElementById('map'), opts);
-		map.setMapTypeId(google.maps.MapTypeId.HYBRID);
-		map.fitBounds(mapBounds);
 		var maptiler = new klokantech.MapTilerMapType(map, mapGetTile, mapBounds, mapMinZoom, mapMaxZoom);
 		var opacitycontrol = new klokantech.OpacityControl(map, maptiler);
+		
+		
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(40.725, -73.985),
+			map: map,
+			icon: '/wp-content/themes/icph/img/eras/progressive/marker.png'
+		});
+		
+		var popup = new google.maps.InfoWindow({
+		    content: "Hello"
+		});
+		
+		google.maps.event.addListener(marker, 'click', function(e) {
+		    // you can use event object as 'e' here
+		    popup.open(map, this);
+		});
+		
+		
 	</script>
 
 <?php
