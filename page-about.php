6bd3dee07048d8b2729987834e9345fa26972e31
Josh Reisner <josh@joshreisner.com>
2013-04-09 18:04:16 -0700

    static mockups for #101

---
+++ page-about.php
@@ -0,0 +1,37 @@
+<?php
+//era landing pages & about page
+get_header();
+?>
+
+<div id="era" class="about">
+
+	<div class="row header">
+		<div class="inner">
+			<h1>The Project</h1>
+			<p>Minim echo park helvetica, thundercats bespoke culpa do vinyl pour-over synth yr lomo gentrify. Pour-over high life commodo shoreditch. Artisan deep v fanny pack, incididunt adipisicing elit cred messenger bag flexitarian photo booth. Aliqua ea nisi four loko ugh next level. 3 wolf moon flexitarian fingerstache small batch. Pinterest odio polaroid, fingerstache post-ironic stumptown selvage pariatur eiusmod. Sint thundercats enim letterpress reprehenderit organic aute.</p>
+		</div>
+	</div>
+	
+	<div class="row feature_policies">
+		<div class="inner">
+			<h3>The Institute for Children, Poverty, and Homelessness (ICPH)</h3>
+			<div class="wp-caption alignright" style="width: 300px">
+				<iframe width="300" height="169" src="http://www.youtube.com/embed/fO_1fclJ35I" frameborder="0" allowfullscreen></iframe>
+				<p class="wp-caption-text">Ethan Sribnick discusses the historical perspective on Poverty and Homelessness</p>
+			</div>
+			<p>Flexitarian hashtag fixie qui consequat ethical. Fashion axe 3 wolf moon master cleanse next level excepteur, craft beer odio pork belly deserunt butcher disrupt. Mumblecore qui fashion axe, biodiesel ut 3 wolf moon locavore eu try-hard bespoke sartorial. 8-bit aliquip brooklyn, non chambray actually gentrify keffiyeh. Banjo viral sustainable, chillwave laborum accusamus retro. Quis excepteur wolf in, reprehenderit flexitarian pitchfork portland nulla anim artisan magna PBR biodiesel. Williamsburg vero leggings portland, tofu laboris cliche fixie.</p>
+			<p>Squid YOLO american apparel, street art vero proident 3 wolf moon. Meggings intelligentsia 90's, tattooed polaroid american apparel irure est. Voluptate fap portland, selfies nisi next level hella marfa accusamus readymade. Ugh truffaut irony, 3 wolf moon flexitarian aliqua skateboard seitan trust fund you probably haven't heard of them pinterest flannel beard freegan voluptate. Banksy banjo DIY semiotics, enim ullamco godard tumblr portland chambray tofu hella hashtag irure deserunt. Authentic carles terry richardson sint cray. Kogi photo booth before they sold out, tumblr swag aesthetic proident mcsweeney's elit.</p>
+
+			<h3>The Book</h3>
+			<div class="wp-caption alignright" style="width: 300px">
+				<img src="/wp-content/themes/icph/img/placeholder/book.jpg" width="300" height="201">
+			</div>
+			<p>Flexitarian hashtag fixie qui consequat ethical. Fashion axe 3 wolf moon master cleanse next level excepteur, craft beer odio pork belly deserunt butcher disrupt. Mumblecore qui fashion axe, biodiesel ut 3 wolf moon locavore eu try-hard bespoke sartorial. 8-bit aliquip brooklyn, non chambray actually gentrify keffiyeh. Banjo viral sustainable, chillwave laborum accusamus retro. Quis excepteur wolf in, reprehenderit flexitarian pitchfork portland nulla anim artisan magna PBR biodiesel. Williamsburg vero leggings portland, tofu laboris cliche fixie.</p>
+			<p>Squid YOLO american apparel, street art vero proident 3 wolf moon. Meggings intelligentsia 90's, tattooed polaroid american apparel irure est. Voluptate fap portland, selfies nisi next level hella marfa accusamus readymade. Ugh truffaut irony, 3 wolf moon flexitarian aliqua skateboard seitan trust fund you probably haven't heard of them pinterest flannel beard freegan voluptate. Banksy banjo DIY semiotics, enim ullamco godard tumblr portland chambray tofu hella hashtag irure deserunt. Authentic carles terry richardson sint cray. Kogi photo booth before they sold out, tumblr swag aesthetic proident mcsweeney's elit.</p>
+		</div>
+	</div>
+		
+</div>
+
+<?php 
+get_footer();
