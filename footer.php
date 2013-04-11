1807e6fcc26a85ce501593ad0f1b7d391a3a34f5
Josh Reisner <josh@joshreisner.com>
2013-04-11 12:01:01 -0700

    correcting footer icons for #108

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
 				<div>&copy; <?php echo date("Y")?> Institute for Children, Poverty &amp; Homelessness- All Rights Reserved</div>
 			</div>
 			<ul>
-				<li class="icon"><a target="_blank" href="https://www.facebook.com/InstituteforChildrenandPoverty"><i class="icon-facebook-sign"></i></a></li>
-				<li class="icon"><a target="_blank" href="https://twitter.com/icph_homeless"><i class="icon-twitter-sign"></i></a></li>
+				<li class="icon"><a target="_blank" href="https://www.facebook.com/InstituteforChildrenandPoverty"><i class="icon-facebook-circled"></i></a></li>
+				<li class="icon"><a target="_blank" href="https://twitter.com/icph_homeless"><i class="icon-twitter-circled"></i></a></li>
 				<li><a href="/about/">About</a></li>
 				<li><a href="/about/#contact">Contact</a></li>
 			</ul>				
