9337e5961a4191e34291b6db717d1e1034426afd
Josh Reisner <josh@joshreisner.com>
2013-08-14 10:43:09 -0700

    adding britta's files and home page styling for #152

--- home.php
+++ home.php
@@ -14,10 +14,10 @@ get_header();
 			<h1><?php echo $era->start_year . '&ndash;' . $era->end_year?></h1>
 			<h2><?php echo $era->post_title?></h2>
 			<ul>
-				<li><a href="#">Read about the Progressive Era</a></li>
-				<li><a href="#">Access the Timeline</a></li>
-				<li><a href="#">Mapping the Settlement Houses</a></li>
-				<li><a href="#">Life in New York Slums: the Gordon Story</a></li>
+				<li><a href="#"><i class="icon-right-circle"></i>Read about the Progressive Era</a></li>
+				<li><a href="#"><i class="icon-right-circle"></i>Access the Timeline</a></li>
+				<li><a href="#"><i class="icon-right-circle"></i>Mapping the Settlement Houses</a></li>
+				<li><a href="#"><i class="icon-right-circle"></i>Life in New York Slums: the Gordon Story</a></li>
 			</ul>
 		</div>
 	</div>
