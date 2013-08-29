5537f7551689ca001fd30e61d38f2bffcfff27df
Jeff Tancil <jtancil@yahoo.com>
2013-08-29 09:15:13 -0400

    Update static content to About page
    Ultimately will have to draw content from WP, but this is a hack temp
    thing

--- page-about.php
+++ page-about.php
@@ -8,7 +8,7 @@ get_header();
 	<div class="row header">
 		<div class="inner">
 			<h1>The Project</h1>
-			<p>Minim echo park helvetica, thundercats bespoke culpa do vinyl pour-over synth yr lomo gentrify. Pour-over high life commodo shoreditch. Artisan deep v fanny pack, incididunt adipisicing elit cred messenger bag flexitarian photo booth. Aliqua ea nisi four loko ugh next level. 3 wolf moon flexitarian fingerstache small batch. Pinterest odio polaroid, fingerstache post-ironic stumptown selvage pariatur eiusmod. Sint thundercats enim letterpress reprehenderit organic aute.</p>
+			<p>PovertyHistory.org provides an intimate look at the experience of poverty over New York City’s history. The site investigates public policies that have shaped the lives of the poor and homeless and shows how the past continues to influence the present. With comprehensive timelines, analytical maps, striking images, primary sources, and informative essays, this site is geared toward students, teachers, service providers, and policymakers with an interest in the history of these intractable issues.</p>
 		</div>
 	</div>
 	
@@ -19,15 +19,50 @@ get_header();
 				<iframe width="300" height="169" src="http://www.youtube.com/embed/fO_1fclJ35I" frameborder="0" allowfullscreen></iframe>
 				<p class="wp-caption-text">Ethan Sribnick discusses the historical perspective on Poverty and Homelessness</p>
 			</div>
-			<p>Flexitarian hashtag fixie qui consequat ethical. Fashion axe 3 wolf moon master cleanse next level excepteur, craft beer odio pork belly deserunt butcher disrupt. Mumblecore qui fashion axe, biodiesel ut 3 wolf moon locavore eu try-hard bespoke sartorial. 8-bit aliquip brooklyn, non chambray actually gentrify keffiyeh. Banjo viral sustainable, chillwave laborum accusamus retro. Quis excepteur wolf in, reprehenderit flexitarian pitchfork portland nulla anim artisan magna PBR biodiesel. Williamsburg vero leggings portland, tofu laboris cliche fixie.</p>
-			<p>Squid YOLO american apparel, street art vero proident 3 wolf moon. Meggings intelligentsia 90's, tattooed polaroid american apparel irure est. Voluptate fap portland, selfies nisi next level hella marfa accusamus readymade. Ugh truffaut irony, 3 wolf moon flexitarian aliqua skateboard seitan trust fund you probably haven't heard of them pinterest flannel beard freegan voluptate. Banksy banjo DIY semiotics, enim ullamco godard tumblr portland chambray tofu hella hashtag irure deserunt. Authentic carles terry richardson sint cray. Kogi photo booth before they sold out, tumblr swag aesthetic proident mcsweeney's elit.</p>
+			<p>The Institute for Children, Poverty, and Homelessness (ICPH) is an independent nonprofit research and policy-analysis organization whose work illuminates the complex issue of family homelessness and informs and enhances public policy related to homeless families, especially children.</p>
+
+<p>Child and family poverty in New York City are not new. Destitute families and homeless children have existed throughout American history, especially in urban areas. This long history of poverty, and of public and private relief efforts, continues to offer lessons for those shaping public policy today. For this reason, ICPH has made a commitment—unique among research organizations of its kind—to in-depth historical research on family poverty and homelessness in New York City.
+</p>
+
+<p>To learn about related ICPH projects, go to <a href="http://www.icphusa.org">www.ICPHusa.org</a></p>
 
 			<h3>The Book</h3>
 			<div class="wp-caption alignright" style="width: 300px">
 				<img src="/wp-content/themes/icph/img/placeholder/book.jpg" width="300" height="201">
 			</div>
-			<p>Flexitarian hashtag fixie qui consequat ethical. Fashion axe 3 wolf moon master cleanse next level excepteur, craft beer odio pork belly deserunt butcher disrupt. Mumblecore qui fashion axe, biodiesel ut 3 wolf moon locavore eu try-hard bespoke sartorial. 8-bit aliquip brooklyn, non chambray actually gentrify keffiyeh. Banjo viral sustainable, chillwave laborum accusamus retro. Quis excepteur wolf in, reprehenderit flexitarian pitchfork portland nulla anim artisan magna PBR biodiesel. Williamsburg vero leggings portland, tofu laboris cliche fixie.</p>
-			<p>Squid YOLO american apparel, street art vero proident 3 wolf moon. Meggings intelligentsia 90's, tattooed polaroid american apparel irure est. Voluptate fap portland, selfies nisi next level hella marfa accusamus readymade. Ugh truffaut irony, 3 wolf moon flexitarian aliqua skateboard seitan trust fund you probably haven't heard of them pinterest flannel beard freegan voluptate. Banksy banjo DIY semiotics, enim ullamco godard tumblr portland chambray tofu hella hashtag irure deserunt. Authentic carles terry richardson sint cray. Kogi photo booth before they sold out, tumblr swag aesthetic proident mcsweeney's elit.</p>
+			<p>Conditions that perpetuate homelessness and poverty today have deep roots in America’s past. In <em>The Poor Among Us: A History of Family Poverty and Homelessness in New York City</em>, Ralph da Costa Nunez and Ethan G. Sribnick explore the world of New York’s poor children and families, from the era of European settlement to the present day: their physical and social environments, the causes of their poverty, and the institutions and social movements that evolved to improve and regulate their lives. This comprehensive history examines the successes and failures of past efforts to reduce poverty and homelessness, providing the historical context that is often lacking in contemporary policy debates.
+
+<p>Published by White Tiger Press, the book is available for purchase at Amazon.com or whitetigerpress.org.</p>
+			<h3>Contact Us</h3>
+
+			<p>Poveryhistory.org is a project of the <em>Institute for Children, Poverty, and Homelessness</em>.</p>
+
+			<p>Ralph Da Costa Nunez, President and CEO</p>
+
+			<p>Dona Anderson, Director</p>
+
+			<p>Ethan G. Sribnick, Senior Research Associate</p>
+
+			<p>Phone: 212-358-8086</p>
+
+			<p>Email: <a href="mailto:info@ICPHusa.org">info@ICPHusa.org</p>
+ 
+
+            <p>44 Cooper Square, 4th Floor</p>
+            
+            <p>New York, NY 10003</p>
+            
+            <p><a href="http://www.ICPHusa.org">www.ICPHusa.org</p>
+
+ 
+
+           	<h3>Media, Participation in Conferences or Events, and Partnership Inquiries</h3>
+            
+            <p>Linda Bazerjian, Director of External Affairs</p>
+            
+            <p>212-358-8086 ext. 1204; <a href="mailto:Lbazerjian@ICPHusa.org">Lbazerjian@ICPHusa.org</p>
+
+
 		</div>
 	</div>
 		
