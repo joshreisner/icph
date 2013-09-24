13ac3dc6f490ae1c7a55ef450ad277c3c5f42c90
Josh Reisner <josh@joshreisner.com>
2013-09-24 11:34:48 -0700

    adding google analytics code to close #182

--- footer.php
+++ footer.php
@@ -110,5 +110,14 @@ $site_title = str_replace('NYC', '<span>NYC</span>', $site_title);
 
 		<?php wp_footer(); ?>
 		<script src="<?php bloginfo('template_directory');?>/js/global.min.js"></script>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-44217931-1', 'povertyhistory.org');
+		  ga('send', 'pageview');
+		</script>
 	</body>
 </html>
