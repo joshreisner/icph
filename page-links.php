dd3f68a50c56da2ab565929a2d803dc611f58a69
Josh Reisner <josh@joshreisner.com>
2013-09-27 12:27:19 -0700

    … and closing the list tags now

--- page-links.php
+++ page-links.php
@@ -5,7 +5,7 @@ $posts = $wpdb->get_results('SELECT id, post_title FROM wp_posts WHERE post_cont
 
 foreach ($posts as $post) { ?>
 
-	<li><a href="/wp-admin/post.php?post=<?php echo $post->id?>&action=edit"><?php echo $post->post_title?></a>
+	<li><a href="/wp-admin/post.php?post=<?php echo $post->id?>&action=edit"><?php echo $post->post_title?></a></li>
 
 <?php } ?>
 </ol>
