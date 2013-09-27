77f9ddfe501f4b45bc85185cb9def114bd2b7c22
Josh Reisner <josh@joshreisner.com>
2013-09-27 12:25:02 -0700

    adding special link-detector page to fix broken links for #184

---
+++ page-links.php
@@ -0,0 +1,11 @@
+<ol>
+<?php
+
+$posts = $wpdb->get_results('SELECT id, post_title FROM wp_posts WHERE post_content LIKE \'%/archives%\' and post_status = \'publish\' ORDER BY post_title');
+
+foreach ($posts as $post) { ?>
+
+	<li><a href="http://povertyhistory.dev/wp-admin/post.php?post=<?php echo $post->id?>&action=edit"><?php echo $post->post_title?></a>
+
+<?php } ?>
+</ol>
